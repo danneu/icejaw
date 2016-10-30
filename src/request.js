4a40c269c513fb05394cf295f6836fbb3d4e113b
Dan Neumann <danrodneu@gmail.com>
2016-10-30 16:49:14 -0600

    Use node-fetch

--- src/request.js
+++ src/request.js
@@ -1,42 +1,29 @@
 const Url = require('url')
 const Path = require('path')
 // 3rd
-const fetch = require('isomorphic-fetch')
+const Promise = require('bluebird')
+const fetch = require('node-fetch')
 const File = require('vinyl')
 const chalk = require('chalk')
 
 
 // Returns a promise that resolves into a Vinyl file
 // rejects on non-200 responses
-function request (url) {
-  const route = Url.parse(url).pathname
-  return fetch(url)
-    .then((response) => {
-      if (response.status !== 200) {
-        console.error(`${chalk.red(response.status)} ${route}`)
-        throw new Error(`<${url}> did not respond with 200`)
-      }
-      return response.text()
-    })
-    .then((text) => {
-      const contents = Buffer.from(text)
-      let path = route
-      if (path.endsWith('/')) {
-        path += 'index.html'
-      }
-      if (!Path.extname(path)) {
-        path += '.html'
-      }
-      console.log(`${chalk.green(200)} ${route} ${chalk.grey('->')} ${path}`)
-      const file = new File({
-        cwd: '/',
-        base: '/',
-        path,
-        contents
-      })
-      return file
-    })
+module.exports = function request (url) {
+  let route = Url.parse(url).pathname
+  return Promise.try(() => {
+    return fetch(url)
+  }).then((response) => {
+    if (response.status !== 200) {
+      console.error(`${chalk.red(response.status)} ${route}`)
+      throw new Error(`<${url}> did not respond with 200`)
+    }
+    return response.buffer()
+  }).then((contents) => {
+    let path = route
+    if (path.endsWith('/')) path += 'index.html'
+    if (!Path.extname(path)) path += '.html'
+    console.log(`${chalk.green(200)} ${route} ${chalk.grey('->')} ${path}`)
+    return new File({ cwd: '/', base: '/', path, contents })
+  })
 }
-
-
-module.exports = request
